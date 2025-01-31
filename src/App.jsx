4cb287cbe4274a452e3b7a4b35fe36af58b48416
Jorge Chan <jorgeluis.chanchan@gmail.com>
2025-01-31 17:17:58 -0600

    Permite agregar la funcionalidad para mostrar la información del curso (Ejercicio 1.1).

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,55 @@
+const Header = (course) => {
+  console.log(course)
+  return (
+    <div>
+      <h1>{course.descripcion}</h1>
+    </div>
+  )
+}
+
+const Content = (exercises) => {
+  console.log(exercises)
+  return (
+    <div>
+      <p>{exercises.exercises[0].part} {exercises.exercises[0].exercise}</p>
+      <p>{exercises.exercises[1].part} {exercises.exercises[1].exercise}</p>
+      <p>{exercises.exercises[2].part} {exercises.exercises[2].exercise}</p>
+    </div>
+  )
+}
+
+const Total = (exercises) => {
+  return (
+    <div>
+      <p>Number of exercises exercises {exercises.totales[0] + exercises.totales[1] + exercises.totales[2]}</p>
+    </div>
+  )
+}
+
+
 const Footer = () => {
   return (
     <div>
-      MyFriends app created by <a href="https://github.com/GeorchitoChan">GeorchitoChan</a>
+      CourseInformation App created by <a href="https://github.com/GeorchitoChan">GeorchitoChan</a>
     </div>
   )
 }
 
 const App = () => {
-  const friends = [
-    { name: 'Peter', age: 4 },
-    { name: 'Maya', age: 10 }
+  const course = 'Half Stack application development'
+  const exercises = [
+    {part: 'Fundamentals of React', exercise: 10},
+    {part: 'Using props to pass data', exercise: 7},
+    {part: 'State of a component', exercise: 14}
   ]
+
+  const totales = exercises.map(item => item.exercise);
+
   return (
     <div>
-      <h1>My Friends</h1>
-      <p>{friends[0].name} {friends[0].age}</p>
-      <p>{friends[1].name} {friends[1].age}</p>
-      <p>{friends}</p>
+      <Header descripcion={course} />
+      <Content exercises={exercises} />
+      <Total totales={totales} />
 
       <Footer />
     </div>
